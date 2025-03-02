58604f510269987e151d1baf730af37012dbcc49
nicorum <nicorum@yandex.ru>
2025-03-02 13:35:09 +0500

    +cors and port fix

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express'
 import { videosRouter } from './videos/videosRouter'
 import { setDB } from './db/db'
+import cors from 'cors'
 
 
 export const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use('/videos', videosRouter)
 
 app.get('/', (req, res) => {
--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 config()
 
 export const SETTINGS = {
-  PORT: process.env.PORT || 3003,
+  PORT: process.env.PORT || 5000,
   PATH: {
     VIDEOS: '/videos'
   }
